9be5672a5f7f75487e4ba4d32dfa397cb4e924d8
koushenhai <2413176044@qq.com>
2023-11-24 15:47:23 +0800

    1

---
+++ dubbo-tech-openfeign/dubbo-tech-openfeign-admin/src/main/java/org/laokoutech/admin/AdminApp.java
@@ -0,0 +1,15 @@
+package org.laokoutech.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class AdminApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApp.class, args);
+    }
+
+}
---
+++ dubbo-tech-openfeign/dubbo-tech-openfeign-user/src/test/java/org/laokoutech/admin/UserAppTests.java
@@ -0,0 +1,13 @@
+package org.laokoutech.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
